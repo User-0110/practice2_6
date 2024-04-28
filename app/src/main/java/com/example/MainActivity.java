26847cda4c16d60cfa6c2c9390c062e051753725
User-0110 <rsfhxhg@gmail.com>
2024-04-28 13:52:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.start_button);
+        textView = findViewById(R.id.result);
+        button.setOnClickListener(this);
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        o -> {
+            if (o.getData() != null) {
+                int res = o.getData().getIntExtra("data", 0);
+                Log.d("tag", "onActivityResult: " + res);
+                textView.setText("Разница чисел: " + res);
+            }
+        }
+    );
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.start_button) {
+            Intent intent = new Intent(this, WorkActivity.class);
+            activityResultLauncher.launch(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/WorkActivity.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.R;
+
+public class WorkActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work);
+        Button work_button = findViewById(R.id.work_button);
+        EditText editText1 = findViewById(R.id.number1);
+        EditText editText2 = findViewById(R.id.number2);
+        EditText editText3 = findViewById(R.id.number3);
+        work_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.work_button) {
+                    if (!editText1.getText().toString().isEmpty() && !editText2.getText().toString().isEmpty() && !editText3.getText().toString().isEmpty()) {
+                        int result = Integer.parseInt(editText1.getText().toString()) - Integer.parseInt(editText2.getText().toString()) - Integer.parseInt(editText3.getText().toString());
+                        Intent intent = new Intent();
+                        intent.putExtra("data", result);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Что-то не так ввели...", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+}
